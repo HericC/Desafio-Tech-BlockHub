d54b64880bc57fc54c76dc789aba038d6d010e10
Heric Macedo Oliveira Silva <heric_dev@hotmail.com>
2021-11-10 16:47:18 -0300

    CHANGE: responsive navbar added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react';
-import { FaSignOutAlt, FaBars } from 'react-icons/fa';
+import React, { useContext, useState } from 'react';
+import { FaSignOutAlt, FaBars, FaWindowClose } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { Nav } from './styled';
 import Logo from '../../images/BlockHubLogo.png';
 import { AuthContext } from '../../contexts/AuthContext';
 
 function Header() {
+  const [mobile, setMobile] = useState(false);
   const { handleLogout } = useContext(AuthContext);
+
+  function handleMobile() {
+    setMobile(!mobile);
+  }
 
   return (
     <Nav>
@@ -16,7 +21,7 @@ function Header() {
         </Link>
       </div>
 
-      <ul>
+      <ul className={mobile ? 'active' : ''}>
         <li>
           <Link to="/#">Cadastrar Usuário</Link>
         </li>
@@ -29,12 +34,21 @@ function Header() {
         <li>
           <Link to="/#">Relatório</Link>
         </li>
+        <li>
+          <Link to="/logout" onClick={handleLogout}>
+            Sair
+          </Link>
+        </li>
       </ul>
 
       <div id="logout">
         <Link to="/logout" onClick={handleLogout}>
           <FaSignOutAlt size={26} />
         </Link>
+      </div>
+
+      <div id="mobile" onClick={handleMobile}>
+        <a>{mobile ? <FaWindowClose size={26} /> : <FaBars size={26} />}</a>
       </div>
     </Nav>
   );
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -12,6 +12,10 @@ const Nav = styled.nav`
 
     li {
       display: flex;
+
+      :last-child {
+        display: none;
+      }
     }
 
     a {
@@ -22,6 +26,7 @@ const Nav = styled.nav`
       border-radius: 1px;
       font-weight: bold;
       color: white;
+      cursor: pointer;
 
       :hover {
         transform: scale(1.1);
@@ -39,12 +44,77 @@ const Nav = styled.nav`
     }
   }
 
-  #logout {
-    margin-left: 120px;
-    margin-right: 30px;
+  #logout,
+  #mobile {
+    margin-left: 100px;
+    margin-right: 50px;
 
     a:hover {
       transform: scale(1.2);
+    }
+  }
+
+  #mobile {
+    display: none;
+  }
+
+  @media (max-width: 940px) {
+    #logo {
+      margin-left: 20px;
+    }
+
+    #logout {
+      margin-left: 60px;
+    }
+  }
+
+  @media (max-width: 860px) {
+    #logout {
+      display: none;
+    }
+
+    #mobile {
+      display: flex;
+      margin-left: 0;
+      margin-right: 20px;
+    }
+
+    ul {
+      width: 100%;
+      position: absolute;
+      top: 60px;
+      left: -100%;
+      opacity: 0;
+      transition: all 500ms;
+      flex-direction: column;
+      background: ${({ theme }) => theme.colors.primaryLight};
+
+      li {
+        border-top: 1px solid ${({ theme }) => theme.colors.primary};
+
+        :first-child {
+          border-top: 0;
+        }
+
+        :last-child {
+          display: flex;
+          background-color: #c3073f;
+        }
+
+        a {
+          justify-content: center;
+          height: 10vw;
+          width: 100%;
+          padding: 30px 10px;
+          border-radius: 0;
+        }
+      }
+    }
+
+    ul.active {
+      left: 0;
+      opacity: 1;
+      z-index: 1;
     }
   }
 `;
